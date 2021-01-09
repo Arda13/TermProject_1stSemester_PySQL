c7ab677b58d5236d25ec734eb8a37ee36081b6dc
Arda Kaya <ardaka23@gmail.com>
2021-01-09 18:22:39 +0300

    progress

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
 
 @app.route("/")
 def home():
-    return render_template("home.html",title="Home")
+    return render_template("home.html",title="Home",posts=posts)
 
 @app.route("/about")
 def about():
